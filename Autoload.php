3c71e70d1711192849aaa2f96d9fbb398bb6ecd4
Phayvanh <bourg.paul@gmail.com>
2015-06-10 00:15:01 +0200

    php blog

--- Autoload.php
+++ Autoload.php
@@ -2,6 +2,7 @@
 
 include 'Global.php';
 
+/* Code d'auto-chargement des fichiers de classes */
 spl_autoload_register(function($class)
 {
     include __DIR__.'/classes/'.str_replace('_','/',$class).'.class.php';
